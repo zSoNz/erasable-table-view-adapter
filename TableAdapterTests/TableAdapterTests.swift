d292e27bc9daa442f25b27521b71e2ca17eafb43
Kikacheishvili Bogdan <bogdan.kikacheishvili@idapgroup.com>
2020-01-17 15:00:46 +0200

    Test on app start added.

--- TableAdapterTests/TableAdapterTests.swift
+++ TableAdapterTests/TableAdapterTests.swift
@@ -6,28 +6,16 @@
 //  Copyright © 2020 Bendis. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 
-class TableAdapterTests: XCTestCase {
+class TableAdapterTests: QuickSpec {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+        describe("TableAdapterTests") {
+            it("should work") {
+                expect(true).to(beTrue())
+            }
         }
     }
-
 }
